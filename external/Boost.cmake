7fff7450e48959ed88f432e6cfef4719708aa993
David Keeney <dkeeney@gmail.com>
2018-09-28 19:25:14 -0700

    A few fixes to the build for Boost.

--- external/Boost.cmake
+++ external/Boost.cmake
@@ -21,6 +21,7 @@
 
 # Creates ExternalProject for building the boost system and filesystem static libraries
 # NOTE: not used by Windows Visual Studio; it uses std::filesystem rather than boost::filesystem.
+#       However, MinGW and MSYS will use this so will need a few fixes.
 #
 # OUTPUT VARIABLES:
 #
@@ -29,10 +30,20 @@
 message(STATUS "  configuring boost build")
 
 
-# Output static library target for linking and dependencies
+# Set some parameters
+
+if (MSYS OR MINGW)
+#set(BOOST_URL "${REPOSITORY_DIR}/external/common/share/boost/boost_1_68_0.zip")
+set(BOOST_URL "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip")
+set(BOOST_HASH "3b1db0b67079266c40b98329d85916e910bbadfc3db3e860c049056788d4d5cd")
+set(bootstrap "bootstrap.bat")
+else()
 #set(BOOST_URL "${REPOSITORY_DIR}/external/common/share/boost/boost_1_68_0.tar.gz")
 set(BOOST_URL "https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz")
 set(BOOST_HASH "da3411ea45622579d419bfda66f45cd0f8c32a181d84adfa936f5688388995cf")
+set(bootstrap "./bootstrap.sh")
+endif()
+
 set(BOOST_COMPONENTS "system filesystem")
 set(BOOST_ROOT "${EP_BASE}/Install/boost")
 set(Boost_INCLUDE_DIR ${BOOST_ROOT}/include)
@@ -46,7 +57,7 @@ ExternalProject_Add( Boost
 	URL ${BOOST_URL}
 	URL_HASH SHA256=${BOOST_HASH}
 	UPDATE_COMMAND ""
-	CONFIGURE_COMMAND ./bootstrap.sh 
+	CONFIGURE_COMMAND ${bootstrap}
 		--with-libraries=filesystem 
 		--with-libraries=system
 		--prefix=${BOOST_ROOT}
