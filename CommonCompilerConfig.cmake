48c744153d4914f5958da1f999fac5e79ced55c2
marty1885 <marty188586@gmail.com>
2019-02-02 09:48:18 +0800

    Build library as shared

--- CommonCompilerConfig.cmake
+++ CommonCompilerConfig.cmake
@@ -279,9 +279,6 @@ else()
 	set(cxx_flags_unoptimized)
 	set(linker_flags_unoptimized)
 
-	# Hide all symbols in DLLs except the ones with explicit visibility;
-        # see https://gcc.gnu.org/wiki/Visibility
-        set(cxx_flags_unoptimized ${cxx_flags_unoptimized} -fvisibility-inlines-hidden -fvisibility=hidden)
         set(cxx_flags_unoptimized ${cxx_flags_unoptimized}  -std=c++${std_ver})
 	
 
