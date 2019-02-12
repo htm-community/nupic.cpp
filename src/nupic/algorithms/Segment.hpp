471deb2284f080c653ef197763cb083d2a427280
Marek Otahal <markotahal@gmail.com>
2019-02-12 12:30:52 +0100

    Revert "OSX missing std::endl"
    This reverts commit 896bbb08673a1044ec9aa9c2f1a9b9f0e6e5eddf.

--- src/nupic/algorithms/Segment.hpp
+++ src/nupic/algorithms/Segment.hpp
@@ -35,7 +35,6 @@
 #include <nupic/math/ArrayAlgo.hpp> // is_sorted
 #include <nupic/math/StlIo.hpp>     // binary_save
 #include <nupic/types/Serializable.hpp>
-#include <nupic/utils/Log.hpp> 
 
 //-----------------------------------------------------------------------
 /**
